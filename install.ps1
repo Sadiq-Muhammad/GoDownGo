0f0d0b3cb9d6e33749c5314418d715760aa63e3b
Sadiq Muhammed <sadmoe000@gmail.com>
2025-02-13 21:51:20 +0300

    Update the windows install script.

--- install.ps1
+++ install.ps1
@@ -9,9 +9,9 @@ $arch = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture
 # Determine the correct binary based on OS and architecture
 if ($os -like "*Windows*") {
     if ($arch -eq "X64") {
-        $DownloadUrl = "https://github.com/Sadiq-Muhammad/GoDo/blob/master/builds/godo-windows-amd64.exe"  # Replace with actual URL for Windows AMD64
+        $DownloadUrl = "https://github.com/Sadiq-Muhammad/GoDo/raw/master/builds/godo-windows-amd64.exe"  # Replace with actual URL for Windows AMD64
     } elseif ($arch -eq "X86") {
-        $DownloadUrl = "https://github.com/Sadiq-Muhammad/GoDo/blob/master/builds/godo-windows-386.exe"  # Replace with actual URL for Windows 32-bit (386)
+        $DownloadUrl = "https://github.com/Sadiq-Muhammad/GoDo/raw/master/builds/godo-windows-386.exe"  # Replace with actual URL for Windows 32-bit (386)
     }
 }
 
@@ -23,7 +23,16 @@ if (!(Test-Path $InstallDir)) {
 # Download the binary if URL is set
 if ($DownloadUrl -ne "") {
     Write-Host "Downloading $BinaryName for $os $arch..."
-    Invoke-WebRequest -Uri $DownloadUrl -OutFile "$InstallDir\$BinaryName$($os -eq 'windows' ? '.exe' : '')"
+    
+    # Fixing the ternary operator with an if condition
+    if ($os -eq 'Windows') {
+        $fileExtension = ".exe"
+    } else {
+        $fileExtension = ""
+    }
+
+    # Download the file
+    Invoke-WebRequest -Uri $DownloadUrl -OutFile "$InstallDir\$BinaryName$fileExtension"
 
     # Add directory to PATH
     $env:Path += ";$InstallDir"
